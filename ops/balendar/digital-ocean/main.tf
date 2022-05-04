3a033ff41ed91b2a84fbaabbbbfcaf0d0e72c591
JoshuaXOng <joshuaong2000@gmail.com>
2022-05-05 07:43:18 +1000

    fix: updated snaphot vol. reference + install gsutils to balendar script

--- ops/balendar/digital-ocean/main.tf
+++ ops/balendar/digital-ocean/main.tf
@@ -34,6 +34,11 @@ resource "null_resource" "attach-balendar-to-gateway" {
     }
 
     inline = [
+      "echo \"deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main\" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list",
+      "curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -",
+      "sudo apt-get update",
+      "sudo apt-get install google-cloud-sdk",
+
       "cd /root/jxo-deployments",
       "chmod 751 ops/balendar/backup-db-volume-job.sh",
       "chmod 751 ops/balendar/snapshot-db-volume.sh",
--- ops/balendar/snapshot-db-volume.sh
+++ ops/balendar/snapshot-db-volume.sh
@@ -1,3 +1,3 @@
 printf -v date '%(%Y-%m-%d-%H:%M:%S)T\n' -1
 
-gsutil -m -o "Credentials:gs_service_key_file=$1" cp -r /var/lib/docker/volumes/balendar_database-data/ gs://$2/databases/docker-volumes/$date
+gsutil -m -o "Credentials:gs_service_key_file=$1" cp -r /var/lib/docker/volumes/prod_database-data/ gs://$2/databases/docker-volumes/$date
