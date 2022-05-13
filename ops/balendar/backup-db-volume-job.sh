42bbfd4a1d0af052b6819229c0770426d81093fb
JoshuaXOng <63457760+JoshuaXOng@users.noreply.github.com>
2022-05-14 01:10:26 +1000

    feat: decrease frequency of cron job

--- ops/balendar/backup-db-volume-job.sh
+++ ops/balendar/backup-db-volume-job.sh
@@ -1,3 +1,3 @@
-echo "0 * * * * /root/jxo-deployments/ops/balendar/snapshot-db-volume.sh /root/balendar-4bd5baf63ecd.json balendar-bucket-main" > backup-db-volume-job
+echo "0 3 * * * /root/jxo-deployments/ops/balendar/snapshot-db-volume.sh /root/balendar-4bd5baf63ecd.json balendar-bucket-main" > backup-db-volume-job
 crontab backup-db-volume-job
 rm backup-db-volume-job
